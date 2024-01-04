059365ee22dc552fe4a0d993f6362638d5ac7102
Siddhartha Chitrakar <siddhartha.chitrakar1@gmail.com>
2024-01-04 16:05:19 -0700

    Calc vectors comments and docstrings

--- calc_vectors.py
+++ calc_vectors.py
@@ -7,7 +7,18 @@ import psycopg2
 
 
 def star_tackles_missed():
-    """Calculate total tackles missed"""
+    '''       
+    Calculates the total 1-star tackles missed
+    
+    For each play, fetch the probability of tackling the ball carrier from all defensive players
+    and retrive the tacklers that had a probability of 0.95 or more but did not make the tackle
+
+    Args: None
+       
+    Returns: None
+    '''
+
+    # Get plays
     model = load("distance.joblib")
     conn = psycopg2.connect("dbname=BigDataBowl user=cschneider")
     cur = conn.cursor()
@@ -15,6 +26,7 @@ def star_tackles_missed():
         cur.execute("UPDATE players SET star_{0}_missed=0".format(i))
     cur.execute("SELECT game_id, play_id, ball_carrier FROM plays")
     plays = cur.fetchall()
+
     for play in plays:
         cur.execute("SELECT x, y, lr, team FROM tracking WHERE game_id=%s AND play_id=%s AND player_id=%s "
                     "AND event='pass_arrived'", play)
@@ -25,6 +37,8 @@ def star_tackles_missed():
             "NOT IN (SELECT player_id FROM tackles WHERE game_id=%s AND play_id=%s AND (tackle='t' OR assist='t')) "
             "AND event='pass_arrived'", list(play[:2]) + [ball_carrier[3]] + list(play[:2]))
         non_tacklers = cur.fetchall()
+                
+        # Calculate the distance of each tackler with the ball carrier, and predict the probability of tackle
         non_tacklers_x = [[math.sqrt(abs(ball_carrier[1] - tackler[2])),
                        math.sqrt(math.sqrt((ball_carrier[0] - tackler[1]) ** 2 + (ball_carrier[1] - tackler[2]) ** 2)),
                        tackler[3], tackler[4]]
@@ -48,7 +62,19 @@ def star_tackles_missed():
 
 
 def star_tackles_made():
-    """Calculate total star tackles made by each player"""
+    '''       
+    Calculate total star tackles made by each player"    
+    
+    For each play, fetch the probability of tackling the ball carrier from all defensive players.
+    Each tackle is given a number of stars based on the probability of making that tackle, 
+    then retrieve the total number star tackles a player has made.
+
+    Args: None
+       
+    Returns: None
+    '''
+        
+    # Get plays
     model = load("distance.joblib")
     conn = psycopg2.connect("dbname=BigDataBowl user=cschneider")
     cur = conn.cursor()
@@ -56,6 +82,7 @@ def star_tackles_made():
         cur.execute("UPDATE players SET star_{0}_made=0".format(i))
     cur.execute("SELECT game_id, play_id, ball_carrier FROM plays")
     plays = cur.fetchall()
+
     for play in plays:
         cur.execute("SELECT x, y, lr, team FROM tracking WHERE game_id=%s AND play_id=%s AND player_id=%s "
                     "AND event='pass_arrived'", play)
@@ -66,11 +93,15 @@ def star_tackles_made():
             "IN (SELECT player_id FROM tackles WHERE game_id=%s AND play_id=%s AND (tackle='t' OR assist='t')) "
             "AND event='pass_arrived'", list(play[:2]) + [ball_carrier[3]] + list(play[:2]))
         tacklers = cur.fetchall()
+
+        # Calculate the distance of each tackler with the ball carrier, and predict the probability of tackle
         tacklers_x = [[math.sqrt(abs(ball_carrier[1] - tackler[2])),
                        math.sqrt(math.sqrt((ball_carrier[0] - tackler[1]) ** 2 + (ball_carrier[1] - tackler[2]) ** 2)),
                        tackler[3], tackler[4]]
                       for tackler in tacklers]
         tackler_probability = model.predict_proba(tacklers_x) if tacklers_x else []
+
+        # Classify each tackle by stars 1-5 based on the probability to make a tackle
         for i, tackler in enumerate(tackler_probability):
             if tackler[1] > 0.90:
                 stars = "1"
@@ -89,14 +120,34 @@ def star_tackles_made():
 
 
 def calculate_vector(magnitude: float, degrees: float):
-    """Calculate the vector of a defender or ball_carrier's speed or acceleration"""
+    '''       
+    Calculate the vector of a defender or ball_carrier's speed or acceleration
+
+    Args:
+        magnitude (float) - magnitude of player
+        degrees (float) - degree the player facing       
+        
+    Returns: the player's speed or acceleration (float)
+    '''
+
     actual_degrees = (450 - degrees) % 360
     rads = actual_degrees * math.pi / 180
     return [magnitude * math.cos(rads), magnitude * math.sin(rads)]
 
 
 def expected_tackles():
-    """Calculate expected tackles"""
+    '''       
+    Calculates the expected tackles for each player
+    
+    For each play, fetch the expectation of a selected player based on metric whether 
+    the tackler is expected to tackle and whether the player actually tackled or not. 
+
+    Args: None
+       
+    Returns: None
+    '''
+
+    # Get plays
     model = load("distance.joblib")
     conn = psycopg2.connect("dbname=BigDataBowl user=cschneider")
     cur = conn.cursor()
@@ -104,6 +155,7 @@ def expected_tackles():
     cur.execute("UPDATE teams SET tackles_above_expected=0")
     cur.execute("SELECT game_id, play_id, ball_carrier FROM plays")
     plays = cur.fetchall()
+
     for play in plays:
         cur.execute("SELECT x, y, lr, team FROM tracking WHERE game_id=%s AND play_id=%s AND player_id=%s "
                     "AND event='pass_arrived'", play)
@@ -118,6 +170,8 @@ def expected_tackles():
                     "NOT IN (SELECT player_id FROM tackles WHERE game_id=%s AND play_id=%s AND (tackle='t' OR assist='t')) "
                     "AND event='pass_arrived'", list(play[:2]) + [ball_carrier[3]] + list(play[:2]))
         non_tacklers = cur.fetchall()
+
+        # Calculate the distance of each player with the ball carrier, and predict the probability of tackle
         tacklers_x = [[math.sqrt(abs(ball_carrier[1] - tackler[2])),
                        math.sqrt(math.sqrt((ball_carrier[0] - tackler[1]) ** 2 + (ball_carrier[1] - tackler[2]) ** 2)),
                        tackler[3], tackler[4]]
@@ -128,12 +182,16 @@ def expected_tackles():
                           for tackler in non_tacklers]
         tackler_probability = model.predict_proba(tacklers_x) if tacklers_x else []
         non_tackler_probability = model.predict_proba(non_tacklers_x)
+        
+        # create two dictionaries between plays that the player tackled or did not, and update accordingly
         tp, ntp = {}, {}
         for i, tackler in enumerate(tackler_probability):
             tp[tacklers[i][0]] = tackler[1]
         for i, tackler in enumerate(non_tackler_probability):
             ntp[non_tacklers[i][0]] = tackler[1]
         ntp = dict(sorted(ntp.items(), key=lambda item: -item[1]))
+
+        # update tackling expectation based on if the tackler is expected to tackle or not, and if the tackler actually tackled or not       
         if not tacklers:
             for nt in ntp:
                 cur.execute("UPDATE players SET tackles_above_expected=tackles_above_expected-%s WHERE id=%s",
@@ -143,8 +201,6 @@ def expected_tackles():
                 break
         else:
             for t in tp:
-                if t == 35466:
-                    print("{0}: {1}".format(play, tp.get(t)))
                 cur.execute("UPDATE players SET tackles_above_expected=tackles_above_expected+%s WHERE id=%s",
                             (1 - tp.get(t), t))
                 cur.execute("UPDATE teams SET tackles_above_expected=tackles_above_expected+%s WHERE name=%s",
@@ -161,7 +217,18 @@ def expected_tackles():
 
 
 def plot_distance():
-    """Plot the distance between a defender and the ball-carrier"""
+    '''       
+    Plot the distance between a defender and the ball-carrier    
+    
+    For each play, fetch the expectation of a selected player based on metric whether 
+    the tackler is expected to tackle and whether the player actually tackled or not. 
+
+    Args: None
+       
+    Returns: None
+    '''
+
+    # Get plays
     conn = psycopg2.connect("dbname=BigDataBowl user=cschneider")
     cur = conn.cursor()
     cur.execute("SELECT game_id, play_id, ball_carrier FROM plays")
@@ -188,33 +255,18 @@ def plot_distance():
                     "AND event='pass_arrived'", list(play[:2]) + [ball_carrier[2]] + list(play[:2]))
         players[1] = cur.fetchall()
         np.seterr(all="raise")
+
+        # calculate the distance from the veritical and lateral distance using pythagoras 
         for i in range(2):
             for player in players[i]:
                 vertical[i].append(math.sqrt(abs(ball_carrier[0] - player[0])))
                 lateral[i].append(math.sqrt(abs(ball_carrier[1] - player[1])))
                 distance[i].append(math.sqrt(math.sqrt((ball_carrier[0] - player[0]) ** 2
                                                        + (ball_carrier[1] - player[1]) ** 2)))
-                #print(ball_carrier)
-                #print(player)
-                """try:
-                    bc_unit_vec = [ball_carrier[4] / np.sqrt(ball_carrier[3] ** 2 + ball_carrier[4] ** 2),
-                                   ball_carrier[3] / np.sqrt(ball_carrier[3] ** 2 + ball_carrier[4] ** 2)]
-                    player_unit_vec = [player[3], player[2]]
-                    vec[i].append(abs((player[0] - ball_carrier[0] - bc_unit_vec[1] * (player[1] - ball_carrier[1])
-                                   / bc_unit_vec[0]) / (bc_unit_vec[1] * player_unit_vec[0] / bc_unit_vec[0]
-                                                        - player_unit_vec[1])))
-                    #print(vec[i][-1])
-                except FloatingPointError:
-                    vec[i].append(100)"""
                 speed[i].append(player[2])
                 acceleration[i].append(player[3])
-    """weights = [[1 / len(distance[0])] * len(distance[0])] + [[1 / len(distance[1])] * len(distance[1])]
-    plt.hist(vertical, weights=weights)
-    plt.show()
-    plt.hist(lateral, weights=weights)
-    plt.show()
-    plt.hist(distance, weights=weights)
-    plt.show()"""
+   
+    # plot the distance
     x = [i for i in zip(lateral[0] + lateral[1], distance[0] + distance[1], speed[0] + speed[1], acceleration[0] + acceleration[1])]
     y = [1] * len(distance[0]) + [0] * len(distance[1])
     print(np.corrcoef(vertical[0] + vertical[1], y))
